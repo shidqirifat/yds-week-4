7890a2cf50658d2c7bc5c7720c2a45599b1c9cf0
shidqirifat <rifatpangestu87@gmail.com>
2021-10-16 17:16:53 +0700

    perbarui add_task

--- add_task.php
+++ add_task.php
@@ -1,18 +1,19 @@
 <?php
-
+$db = new SQLite3('db/todo.db');
 // Cek apakah ada task baru yang diinput oleh user
 if (isset($_POST['input_task'])) {
     $newTask = $_POST['input_task'];
+    var_dump(($newTask));
 
     // @TODO: Insert task di database
-    echo "Ops.. fungsi tidak ditemukan. Buat fungsi untuk insert task di database";
+    $db->exec("INSERT INTO tasks(name, is_done) VALUES ('$newTask', 0)");
 
     //  Function untuk redirect ke halaman index.php
-    //  header('Location: index.php');
+    header('Location: index.php');
 } else {
     /***
      * Langsung dilakukan redirect ke halaman index.php
      * Jika user melakukan input kosong
-    ***/
+     ***/
     header('Location: index.php');
 }
